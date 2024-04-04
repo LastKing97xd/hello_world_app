7c3ad667a8de3c54533ecf8a5091032c150b5860
DhakAres <davidanthonyhidalgo@gmail.com>
2024-04-04 15:00:37 -0500

    Antes de Separar Botones

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Contador', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600))),
+        title: const Center(
+            child: Text('Contador',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600))),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Clicks', style: TextStyle(fontSize: 25))
-          ],
-        )
-      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('$clickCounter',
+              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+          Text('Click${clickCounter == 1 ? '' : 's'}',
+              style: const TextStyle(fontSize: 25))
+          // if (clickCounter == 1)
+          //   const Text('Click',
+          //       style: TextStyle(fontSize: 25)),
+          // if (clickCounter != 1)
+          //   const Text('Clicks',
+          //       style: TextStyle(fontSize: 25))
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
+        onPressed: () {
+          setState(() {
+            //clickCounter = clickCounter + 1;
+            clickCounter++;
+          });
         },
-        child: const Icon( Icons.plus_one ),
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
